1f377ae83da5f104fcb1c1c36994643a6742ab41
SeonjuKim <o6o4_@naver.com>
2021-12-03 00:24:07 +0900

    [Feat] Webpack 이해 (구구단 웹게임 Webpack으로 구현)

---
+++ Gugudan/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const { webpack } = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval', 
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+
+    entry: {
+        app: ['./client'],
+    },
+
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 5% in KR', 'last 2 chrome versions']
+                        }
+                    }],
+                    '@babel/preset-react'
+                ],
+            },
+        }],
+    },
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js'
+    }
+};
