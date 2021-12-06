714b7384186a2a16ede835f5b1a574fb758f6f13
SeonjuKim <o6o4_@naver.com>
2021-12-06 18:34:39 +0900

    [Feat] 반응속도체크 웹게임 Hooks로 구현

--- ResponseCheck/client.jsx
+++ ResponseCheck/client.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
-const ResponseCheck = require('../ResponseCheck/ResponseCheck');
+const ResponseCheckHooks = require('./ResponseCheckHooks');
 
-ReactDom.render(<ResponseCheck />, document.querySelector('#root'));
+ReactDom.render(<ResponseCheckHooks />, document.querySelector('#root'));
