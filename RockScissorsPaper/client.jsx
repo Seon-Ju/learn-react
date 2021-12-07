bf21537e2e5cbe91936c5c03ed5a83107ac584ff
SeonjuKim <o6o4_@naver.com>
2021-12-07 17:15:23 +0900

    [Feat] 가위바위보 웹게임 Class로 구현

---
+++ RockScissorsPaper/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+const RockScissorsPaper = require('./RockScissorsPaper');
+
+ReactDom.render(<RockScissorsPaper />, document.querySelector('#root'));
